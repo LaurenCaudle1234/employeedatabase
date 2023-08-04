6116ca10f649187b03327107bc7bf64896278deb
Lauren Caudle <laurenecaudle@gmail.com>
2023-08-04 11:32:59 -0500

    updates

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+employee_db;
+employee_db;
+
+employee_db;
+
+employee (
+  id AUTO_INCREMENT,
+  employee_name (100) 
+);
+
+ names (
+    id  AUTO_INCREMENT ,
+    employee_id,
+    names,
+    (employee_id)
+    employee(id)
+);
